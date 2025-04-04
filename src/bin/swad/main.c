9832cb328fd493bbf6640441045ddf8c2540faa6
Felix Palmen <felix@palmen-it.de>
2025-04-04 16:05:32 +0200

    main: Fix regression reading config file
    Config file must be read before using any config settings (other than
    the config file location and initial verbosity).

--- src/bin/swad/main.c
+++ src/bin/swad/main.c
@@ -51,7 +51,6 @@ static void prestartup(void *receiver, void *sender, void *args)
 
     PSC_EAStartup *ea = args;
 
-    Config_readConfigFile();
     MW_FormData_setValidation(FDV_UTF8_SANITIZE);
     MW_Session_init();
     Authenticator_init();
@@ -145,11 +144,14 @@ static void shutdown(void *receiver, void *sender, void *args)
 int main(int argc, char **argv)
 {
     if (Config_init(argc, argv) < 0) return EXIT_FAILURE;
-    PSC_RunOpts_init(Config_pidfile());
     PSC_RunOpts_enableDefaultLogging("swad");
+    PSC_Log_setMaxLogLevel(Config_verbose() ? PSC_L_DEBUG : PSC_L_INFO);
+    Config_readConfigFile();
+
+    PSC_RunOpts_init(Config_pidfile());
+    PSC_Log_setMaxLogLevel(Config_verbose() ? PSC_L_DEBUG : PSC_L_INFO);
     PSC_RunOpts_runas(Config_uid(), Config_gid());
     if (Config_foreground()) PSC_RunOpts_foreground();
-    PSC_Log_setMaxLogLevel(Config_verbose() ? PSC_L_DEBUG : PSC_L_INFO);
     PSC_Event_register(PSC_Service_prestartup(), 0, prestartup, 0);
     PSC_Event_register(PSC_Service_shutdown(), 0, shutdown, 0);
     return PSC_Service_run();
