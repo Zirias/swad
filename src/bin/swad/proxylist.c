459356a2067447ff30af2fd9c582796ed16269ba
Felix Palmen <felix@palmen-it.de>
2025-04-07 22:01:35 +0200

    ProxyList: Parse proxy headers backwards
    Parsing proxy headers backwards is the best possibility to sidestep
    "Forwarded header sabotage"[1] by some rogue proxy inserting
    intentionally invalid values.
    
    [1] https://adam-p.ca/blog/2022/03/forwarded-header-sabotage/

--- src/bin/swad/proxylist.c
+++ src/bin/swad/proxylist.c
@@ -14,6 +14,8 @@
 
 #define CTXKEY "_PROXYLIST"
 
+#define arraysz(x) (sizeof (x) / sizeof *(x))
+
 struct RemoteEntry
 {
     char *addr;
@@ -42,131 +44,263 @@ static void deleteProxyList(void *obj)
     PSC_List_destroy(obj);
 }
 
-static void findFor(const char **p, size_t *l)
+static void stripRfc7239(char **entry, size_t *len)
 {
-    if (!*l) return;
-    if (*l < 5) goto notFound;
-    size_t s = 0;
-    for (; s < *l - 5; ++s)
+    if (!*len) return;
+    int quote = 0;
+    int esc = 0;
+    size_t comp = 0;
+    for (size_t i = 0; i < *len; ++i)
     {
-	if (!strncasecmp(*p + s, "for=", 4))
+	if (esc)
 	{
-	    s += 4;
-	    *l -= s;
-	    *p += s;
-	    s = 0;
-	    for (; s < *l; ++s)
-	    {
-		if ((*p)[s] == ';') break;
-	    }
-	    *l = s;
-	    return;
+	    esc = 0;
+	    continue;
 	}
-    }
 
-notFound:
-    *l = 0;
-}
-
-static void stripAddr(const char **p, size_t *l)
-{
-    if (!*l) return;
-    size_t s;
-
-    if (**p == '[')
-    {
-	for (s = 1; s < *l; ++s)
-	{
-	    if ((*p)[s] == ']')
-	    {
-		*l = s - 1;
-		++*p;
-		return;
-	    }
-	}
-	*l = 0;
-	return;
-    }
-
-    size_t ncolons = 0;
-    const char *lastcolon = 0;
-    for (s = 0; s < *l; ++s)
-    {
-	if ((*p)[s] == ':')
-	{
-	    lastcolon = *p + s;
-	    ++ncolons;
-	}
-    }
-
-    if (ncolons == 1) *l = (lastcolon - *p);
-}
-
-static char *parseAddr(const char **p, int rfc7239)
-{
-    char buf[1024];
-    size_t buflen = 0;
-    int quot = 0;
-
-    if (!**p) return 0;
-
-    const char *rp = *p;
-    for (; buflen < sizeof buf && *rp; ++rp)
-    {
-	switch (*rp)
+	switch ((*entry)[i])
 	{
 	    case '"':
-		quot = !quot;
+		quote = !quote;
 		break;
 
-	    case ',':
-		if (!quot) goto entryEnd;
+	    case '\\':
+		esc = 1;
+		break;
+
+	    case ';':
+		if (!quote)
+		{
+		    (*entry)[i] = 0;
+		    if (!strncasecmp(*entry + comp, "for=", 4))
+		    {
+			comp += 4;
+			*len = i - comp;
+			*entry += comp;
+			return;
+		    }
+		}
+		comp = ++i;
 		ATTR_FALLTHROUGH;
 
 	    default:
-		buf[buflen++] = *rp;
+		break;
 	}
     }
 
-entryEnd:
-    if (*rp && *rp != ',')
+    if (comp < *len && !strncasecmp(*entry + comp, "for=", 4))
     {
-	PSC_Log_msg(PSC_L_WARNING, "proxylist: overlong entry, truncating");
-	while (*rp && *rp != ',') ++rp;
+	comp += 4;
+	*len -= comp;
+	*entry += comp;
     }
-    if (*rp == ',')
+    else *len = 0;
+}
+
+static char *parseEntry(char *entry, size_t len, int rfc7239)
+{
+    char buf[512];
+
+    if (rfc7239) stripRfc7239(&entry, &len);
+    if (!len) return 0;
+
+    int quote = 0;
+    int esc = 0;
+    int bracket = 0;
+    unsigned buflen = 0;
+    unsigned ncolons = 0;
+    unsigned lastcolon = 0;
+
+    for (size_t i = 0; i < len; ++i)
     {
-	++rp;
-	while (*rp && (*rp == ' ' || *rp == '\t')) ++rp;
+	if (esc)
+	{
+	    esc = 0;
+	    buf[buflen++] = entry[i];
+	    continue;
+	}
+
+	switch (entry[i])
+	{
+	    case '\\':
+		esc = 1;
+		break;
+
+	    case '"':
+		quote = !quote;
+		break;
+
+	    case '[':
+		if (bracket >= 0) ++bracket;
+		else goto takechar;
+		break;
+
+	    case ']':
+		if (bracket > 0)
+		{
+		    if (!--bracket) goto done;
+		}
+		else goto takechar;
+		break;
+
+	    case ':':
+		lastcolon = buflen;
+		++ncolons;
+		ATTR_FALLTHROUGH;
+
+	    default:
+		if (!bracket) bracket = -1;
+	    takechar:
+		buf[buflen++] = entry[i];
+		break;
+	}
     }
-    *p = rp;
 
-    const char *bufp = buf;
-    if (rfc7239) findFor(&bufp, &buflen);
-    stripAddr(&bufp, &buflen);
+done:
+    if (ncolons == 1) buflen = lastcolon;
+    buf[buflen] = 0;
 
-    if (!buflen) return 0;
-    char *addr = PSC_malloc(buflen + 1);
-    memcpy(addr, bufp, buflen);
-    addr[buflen] = 0;
-    return addr;
+    char *ret = 0;
+    if (buflen)
+    {
+	ret = PSC_malloc(buflen + 1);
+	memcpy(ret, buf, buflen + 1);
+    }
+    return ret;
+}
+
+static int escaped(char *buf, size_t i)
+{
+    size_t bscount = 0;
+    while (i-- > 0)
+    {
+	if (buf[i] != '\\') break;
+	++bscount;
+    }
+    return bscount % 2;
+}
+
+static int hdrEntry(char **start, size_t *len, char *buf, size_t *buflen,
+	int rfc7239)
+{
+    int quote = 0;
+    size_t i = *buflen;
+    while (i > 0)
+    {
+	switch (buf[--i])
+	{
+	    case '"':
+		if (rfc7239 && !escaped(buf, i)) quote = !quote;
+		break;
+
+	    case ',':
+		if (!quote)
+		{
+		    buf[i] = 0;
+		    size_t newlen = i;
+		    while (buf[++i] == ' ');
+		    *len = *buflen - i;
+		    *start = buf + i;
+		    *buflen = newlen;
+		    return 0;
+		}
+		ATTR_FALLTHROUGH;
+
+	    default:
+		break;
+	}
+    }
+
+    if (!quote)
+    {
+	*len = *buflen;
+	*buflen = 0;
+	*start = buf;
+	return 0;
+    }
+
+    return -1;
+}
+
+static int parseHdrVal(unsigned *parsed, char **addr, unsigned n,
+	const char *val, int rfc7239)
+{
+    char buf[8192];
+    size_t hdrlen = strlen(val);
+    if (hdrlen >= arraysz(buf))
+    {
+	size_t shift = hdrlen - arraysz(buf) + 1;
+	hdrlen -= shift;
+	val += shift;
+    }
+    memcpy(buf, val, hdrlen);
+    buf[hdrlen] = 0;
+
+    int rc = 0;
+    unsigned ok = 0;
+    char *entry;
+    size_t entryLen;
+    while (hdrlen && ok < n)
+    {
+	if (hdrEntry(&entry, &entryLen, buf, &hdrlen, rfc7239) < 0)
+	{
+	    rc = -1;
+	    goto done;
+	}
+	char *entryAddr = parseEntry(entry, entryLen, rfc7239);
+	if (!entryAddr)
+	{
+	    rc = -1;
+	    goto done;
+	}
+	addr[ok++] = entryAddr;
+    }
+
+done:
+    *parsed = ok;
+    return rc;
 }
 
 static PSC_List *proxyAddr(HeaderIterator *i, int rfc7239)
 {
-    PSC_List *proxyList = 0;
+    const char *hdrVal[16];
+    size_t hdrValCount = 0;
+    unsigned lastHdrVal;
+
     while (HeaderIterator_moveNext(i))
     {
-	const char *val = Header_value(HeaderIterator_current(i));
-	char *addr;
-	while ((addr = parseAddr(&val, rfc7239)))
+	lastHdrVal = hdrValCount++ % arraysz(hdrVal);
+	hdrVal[lastHdrVal] = Header_value(HeaderIterator_current(i));
+    }
+    if (hdrValCount > arraysz(hdrVal)) hdrValCount = arraysz(hdrVal);
+
+    char *addr[16];
+    unsigned addrCount = 0;
+
+    while (hdrValCount--)
+    {
+	unsigned parsed;
+	int rc = parseHdrVal(&parsed, addr + addrCount,
+		arraysz(addr) - addrCount, hdrVal[lastHdrVal], rfc7239);
+	addrCount += parsed;
+	if (rc < 0 || !parsed) break;
+	if (addrCount == arraysz(addr)) break;
+	if (lastHdrVal) --lastHdrVal;
+	else lastHdrVal = arraysz(hdrVal) - 1;
+    }
+
+    if (addrCount)
+    {
+	PSC_List *proxyList = PSC_List_create();
+	while (addrCount)
 	{
-	    if (!proxyList) proxyList = PSC_List_create();
-	    PSC_List_append(proxyList, createRemoteEntry(addr, 0),
+	    PSC_List_append(proxyList, createRemoteEntry(addr[--addrCount], 0),
 		    deleteRemoteEntry);
 	}
+	return proxyList;
     }
-    return proxyList;
+
+    return 0;
 }
 
 const PSC_List *ProxyList_get(HttpContext *context)
