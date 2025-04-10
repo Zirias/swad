e55bb5f52613876c6a2a6472530dc2582bff10ee
Felix Palmen <felix@palmen-it.de>
2025-04-10 07:10:43 +0200

    Authenticator: Refactor session properties
    Put everything as a hash table by realm in a single session property
    (User object, hash table of blocked user names and rate limiter for
    failed logins). This also scopes rate limiting to the active realm.

--- src/bin/swad/authenticator.c
+++ src/bin/swad/authenticator.c
@@ -9,14 +9,17 @@
 #include <string.h>
 
 #define SESSKEY "swad_authinfo"
-#define SESSBLOCKKEY "swad_auth_blocked"
-#define SESSLIMITKEY "swad_auth_limits"
+
+typedef struct AuthInfo
+{
+    User *user;
+    PSC_HashTable *blocks;
+    RateLimit *failLimit;
+} AuthInfo;
 
 struct Authenticator
 {
-    Session *session;
-    PSC_HashTable *authinfo;
-    PSC_HashTable *blockinfo;
+    PSC_HashTable *authInfos;
     const char *realm;
 };
 
@@ -62,7 +65,17 @@ static void deleteUser(void *obj)
     free(user);
 }
 
-static void deleteHT(void *obj)
+static void deleteAuthInfo(void *obj)
+{
+    if (!obj) return;
+    AuthInfo *self = obj;
+    RateLimit_destroy(self->failLimit);
+    PSC_HashTable_destroy(self->blocks);
+    deleteUser(self->user);
+    free(self);
+}
+
+static void deleteAuthInfos(void *obj)
 {
     PSC_HashTable_destroy(obj);
 }
@@ -72,42 +85,43 @@ static void deleteRealmConfig(void *obj)
     PSC_List_destroy(obj);
 }
 
-static PSC_HashTable *getAuthInfo(Session *session)
+static PSC_HashTable *getAuthInfos(Session *session)
 {
-    PSC_HashTable *authinfo = Session_getProp(session, SESSKEY);
-    if (!authinfo)
+    PSC_HashTable *authInfos = Session_getProp(session, SESSKEY);
+    if (!authInfos)
     {
-	authinfo = PSC_HashTable_create(4);
-	Session_setProp(session, SESSKEY, authinfo, deleteHT);
+	authInfos = PSC_HashTable_create(4);
+	Session_setProp(session, SESSKEY, authInfos, deleteAuthInfos);
     }
-    return authinfo;
-}
-
-static PSC_HashTable *getBlockInfo(Session *session)
-{
-    PSC_HashTable *blockinfo = Session_getProp(session, SESSBLOCKKEY);
-    if (!blockinfo)
-    {
-	blockinfo = PSC_HashTable_create(4);
-	Session_setProp(session, SESSBLOCKKEY, blockinfo, deleteHT);
-    }
-    return blockinfo;
+    return authInfos;
 }
 
 Authenticator *Authenticator_create(Session *session, const char *realm)
 {
     Authenticator *self = PSC_malloc(sizeof *self);
-    self->session = session;
-    self->authinfo = getAuthInfo(session);
-    self->blockinfo = getBlockInfo(session);
+    self->authInfos = getAuthInfos(session);
     self->realm = realm ? realm : DEFAULT_REALM;
     return self;
+}
+
+static AuthInfo *getAuthInfo(Authenticator *self, int create)
+{
+    AuthInfo *authInfo = PSC_HashTable_get(self->authInfos, self->realm);
+    if (create && !authInfo)
+    {
+	authInfo = PSC_malloc(sizeof *authInfo);
+	memset(authInfo, 0, sizeof *authInfo);
+	PSC_HashTable_set(self->authInfos, self->realm,
+		authInfo, deleteAuthInfo);
+    }
+    return authInfo;
 }
 
 const User *Authenticator_user(const Authenticator *self)
 {
     pthread_mutex_lock(&authlock);
-    User *user = PSC_HashTable_get(self->authinfo, self->realm);
+    AuthInfo *authInfo = getAuthInfo((Authenticator *)self, 0);
+    User *user = authInfo ? authInfo->user : 0;
     pthread_mutex_unlock(&authlock);
     return user;
 }
@@ -123,50 +137,44 @@ int Authenticator_silentLogin(Authenticator *self)
     pthread_mutex_lock(&realmlock);
     PSC_HashTableIterator *i = 0;
     PSC_ListIterator *j = 0;
-    User *user = PSC_HashTable_get(self->authinfo, self->realm);
-    if (user) goto done;
+    AuthInfo *authInfo = getAuthInfo(self, 0);
+    if (!authInfo || authInfo->user) goto done;
     PSC_List *realmConfig = PSC_HashTable_get(realms, self->realm);
     if (!realmConfig || PSC_List_size(realmConfig) == 0) goto done;
     j = PSC_List_iterator(realmConfig);
-    for (i = PSC_HashTable_iterator(self->authinfo);
+    for (i = PSC_HashTable_iterator(self->authInfos);
 	    PSC_HashTableIterator_moveNext(i); )
     {
-	const User *otherUser = PSC_HashTableIterator_current(i);
+	const AuthInfo *otherInfo = PSC_HashTableIterator_current(i);
+	if (!otherInfo->user) continue;
 	while (PSC_ListIterator_moveNext(j))
 	{
-	    if (strcmp(otherUser->checker,
+	    if (strcmp(otherInfo->user->checker,
 			PSC_ListIterator_current(j))) continue;
-	    user = copyUser(otherUser);
-	    PSC_HashTable_set(self->authinfo, self->realm, user, deleteUser);
+	    authInfo->user = copyUser(otherInfo->user);
 	    goto done;
 	}
     }
 done:
     PSC_HashTableIterator_destroy(i);
     PSC_ListIterator_destroy(j);
+    int ok = authInfo && authInfo->user;
     pthread_mutex_unlock(&realmlock);
     pthread_mutex_unlock(&authlock);
-    return user ? 1 : 0;
+    return ok;
 }
 
-static void deleteLimit(void *obj)
+static RateLimit *getLimits(Authenticator *self)
 {
-    RateLimit_destroy(obj);
-}
-
-static RateLimit *getLimits(Session *session)
-{
-    RateLimit *authLimits = Session_getProp(session, SESSLIMITKEY);
-    if (!authLimits)
+    AuthInfo *authInfo = getAuthInfo(self, 1);
+    if (!authInfo->failLimit)
     {
 	RateLimitOpts *limitOpts = RateLimitOpts_create(1);
 	RateLimitOpts_addLimit(limitOpts, 15 * 60, 5);
-	authLimits = RateLimit_create(limitOpts);
+	authInfo->failLimit = RateLimit_create(limitOpts);
 	RateLimitOpts_destroy(limitOpts);
-	Session_setProp(session, SESSLIMITKEY,
-		authLimits, deleteLimit);
     }
-    return authLimits;
+    return authInfo->failLimit;
 }
 
 int Authenticator_login(Authenticator *self, const char *user, const char *pw)
@@ -174,11 +182,13 @@ int Authenticator_login(Authenticator *self, const char *user, const char *pw)
     int ok = 0;
     pthread_mutex_lock(&authlock);
     pthread_mutex_lock(&realmlock);
-    if (PSC_HashTable_get(self->blockinfo, user))
+    AuthInfo *authInfo = getAuthInfo(self, 0);
+    if (authInfo && authInfo->blocks &&
+	    PSC_HashTable_get(authInfo->blocks, user))
     {
-	if (RateLimit_check(getLimits(self->session), user))
+	if (RateLimit_check(getLimits(self), user))
 	{
-	    PSC_HashTable_delete(self->blockinfo, user);
+	    PSC_HashTable_delete(authInfo->blocks, user);
 	}
 	else
 	{
@@ -200,8 +210,9 @@ int Authenticator_login(Authenticator *self, const char *user, const char *pw)
 	    char *realname = 0;
 	    if (checker->check(checker, user, pw, &realname))
 	    {
-		PSC_HashTable_set(self->authinfo, self->realm,
-			createUser(user, realname, checkerName), deleteUser);
+		if (!authInfo) authInfo = getAuthInfo(self, 1);
+		deleteUser(authInfo->user);
+		authInfo->user = createUser(user, realname, checkerName);
 		ok = 1;
 	    }
 	}
@@ -210,10 +221,12 @@ int Authenticator_login(Authenticator *self, const char *user, const char *pw)
 
     if (!ok)
     {
-	if (!RateLimit_check(getLimits(self->session), user))
+	if (!RateLimit_check(getLimits(self), user))
 	{
 	    ok = -1;
-	    PSC_HashTable_set(self->blockinfo, user, (void *)1, 0);
+	    if (!authInfo) authInfo = getAuthInfo(self, 1);
+	    if (!authInfo->blocks) authInfo->blocks = PSC_HashTable_create(4);
+	    PSC_HashTable_set(authInfo->blocks, user, (void *)1, 0);
 	}
     }
 
@@ -227,7 +240,12 @@ done:
 void Authenticator_logout(Authenticator *self)
 {
     pthread_mutex_lock(&authlock);
-    PSC_HashTable_delete(self->authinfo, self->realm);
+    AuthInfo *authInfo = getAuthInfo(self, 0);
+    if (authInfo && authInfo->user)
+    {
+	deleteUser(authInfo->user);
+	authInfo->user = 0;
+    }
     pthread_mutex_unlock(&authlock);
 }
 
