452e4dada93ac7ae5f1f374499bb6a5a3fe87f9d
Felix Palmen <felix@palmen-it.de>
2025-04-14 10:55:17 +0200

    Session: Use PSC_Dictionary internally

--- src/bin/swad/middleware/session.c
+++ src/bin/swad/middleware/session.c
@@ -9,7 +9,6 @@
 #include "../http/httpstatus.h"
 #include "../proxylist.h"
 #include "../ratelimit.h"
-#include "../util.h"
 #include "cookies.h"
 
 #include <poser/core.h>
@@ -24,26 +23,12 @@
 #define MAXIDLE 3600
 #define CLEANINTERVAL 900
 
-#define SPHT_BITS 4
-#define SPHT_SIZE HT_SIZE(SPHT_BITS)
-#define SHT_BITS 8
-#define SHT_SIZE HT_SIZE(SHT_BITS)
-
-typedef struct SessionProp SessionProp;
-struct SessionProp
-{
-    const char *name;
-    void *value;
-    SessionProp *next;
-    ObjDeleter deleter;
-};
+#define SID_LEN 43 // 32 bytes in base64
 
 struct Session
 {
-    SessionProp *buckets[SPHT_SIZE];
-    char *id;
+    PSC_Dictionary *props;
     char *referrer;
-    Session *next;
     pthread_mutex_t lock;
     time_t ctime;
     time_t atime;
@@ -52,127 +37,63 @@ struct Session
 #define sessionExpired(s, n) ((n) - (s).atime > MAXIDLE \
 	|| (n) - (s).ctime > MAXAGE)
 
-static Session *buckets[SHT_SIZE];
+static PSC_Dictionary *sessions;
 static RateLimitOpts *createLimitOpts;
 static RateLimit *createLimit;
-static pthread_mutex_t bucklocks[SHT_SIZE];
 static time_t cleantime;
+static pthread_mutex_t sessionlock;
 static pthread_mutex_t cleanlock;
 
-static void clean(time_t now);
-static Session *createSession(time_t now) ATTR_RETNONNULL;
-static void deleteSession(Session *self);
-static SessionProp *findProp(Session *self, const char *key, uint8_t *hashval)
-    CMETHOD ATTR_NONNULL((2)) ATTR_ACCESS((write_only, 3));
-static Session *findSession(const char *id, time_t now, uint8_t *hashval)
-    ATTR_NONNULL((1)) ATTR_ACCESS((write_only, 3));
-static SessionProp *insertOrUpdate(Session *self, const char *key)
-    CMETHOD ATTR_NONNULL((2)) ATTR_RETNONNULL;
-
-static void deleteSession(Session *self)
+static void deleteSession(void *obj)
 {
-    if (!self) return;
-    for (uint8_t p = 0; p < SPHT_SIZE; ++p)
-    {
-	SessionProp *prop = self->buckets[p];
-	SessionProp *next;
-	while (prop)
-	{
-	    next = prop->next;
-	    if (prop->deleter) prop->deleter(prop->value);
-	    free(prop);
-	    prop = next;
-	}
-    }
+    if (!obj) return;
+    Session *self = obj;
     pthread_mutex_destroy(&self->lock);
     free(self->referrer);
-    free(self->id);
+    PSC_Dictionary_destroy(self->props);
     free(self);
 }
 
-static Session *findSession(const char *id, time_t now, uint8_t *hashval)
+static Session *findSession(const char *id, time_t now)
 {
-    uint8_t h = hash(id, SHT_BITS);
-    if (hashval) *hashval = h;
-    pthread_mutex_lock(&bucklocks[h]);
-    Session *session = buckets[h];
-    Session *parent = 0;
-    while (session)
+    Session *session = 0;
+    pthread_mutex_lock(&sessionlock);
+    session = PSC_Dictionary_get(sessions, id, SID_LEN);
+    if (session && sessionExpired(*session, now))
     {
-	if (!strcmp(session->id, id))
-	{
-	    if (sessionExpired(*session, now))
-	    {
-		if (parent) parent->next = session->next;
-		else buckets[h] = session->next;
-		deleteSession(session);
-		session = 0;
-	    }
-	    break;
-	}
-	parent = session;
-	session = session->next;
+	PSC_Dictionary_set(sessions, id, SID_LEN, 0, 0);
+	session = 0;
     }
-    pthread_mutex_unlock(&bucklocks[h]);
+    pthread_mutex_unlock(&sessionlock);
     return session;
 }
 
-static Session *createSession(time_t now)
+static Session *createSession(time_t now, char *id)
 {
-    char *id = 0;
-    uint8_t h;
     do
     {
-	free(id);
-	id = PSC_Random_createStr(32, 0);
-    } while (findSession(id, now, &h));
-    Session *parent = buckets[h];
-    while (parent && parent->next) parent = parent->next;
+	PSC_Random_string(id, SID_LEN+1, 0);
+    } while (findSession(id, now));
     Session *self = PSC_malloc(sizeof *self);
-    memset(self, 0, sizeof *self);
-    self->id = id;
+    self->props = PSC_Dictionary_create(0);
+    self->referrer = 0;
     pthread_mutex_init(&self->lock, 0);
     self->ctime = now;
     self->atime = now;
-    if (parent) parent->next = self;
-    else buckets[h] = self;
+    pthread_mutex_lock(&sessionlock);
+    PSC_Dictionary_set(sessions, id, SID_LEN, self, 0);
+    pthread_mutex_unlock(&sessionlock);
     return self;
 }
 
-static SessionProp *findProp(Session *self, const char *key, uint8_t *hashval)
+static int isExpired(const void *key, size_t keysz, void *obj, const void *arg)
 {
-    uint8_t h = hash(key, SPHT_BITS);
-    if (hashval) *hashval = h;
-    SessionProp *prop = self->buckets[h];
-    while (prop)
-    {
-	if (!strcmp(prop->name, key)) break;
-	prop = prop->next;
-    }
-    return prop;
-}
+    (void)key;
+    (void)keysz;
 
-static SessionProp *insertOrUpdate(Session *self, const char *key)
-{
-    uint8_t hashval;
-    SessionProp *prop = findProp(self, key, &hashval);
-    if (!prop)
-    {
-	prop = PSC_malloc(sizeof *prop);
-	prop->name = key;
-	prop->value = 0;
-	prop->next = 0;
-	prop->deleter = 0;
-	SessionProp *parent = self->buckets[hashval];
-	if (parent)
-	{
-	    while (parent->next) parent = parent->next;
-	    parent->next = prop;
-	}
-	else self->buckets[hashval] = prop;
-    }
-    if (prop->deleter) prop->deleter(prop->value);
-    return prop;
+    Session *session = obj;
+    const time_t *now = arg;
+    return sessionExpired(*session, *now);
 }
 
 static void clean(time_t now)
@@ -185,39 +106,14 @@ static void clean(time_t now)
     }
     cleantime = now;
     pthread_mutex_unlock(&cleanlock);
-    for (unsigned h = 0; h < SHT_SIZE; ++h)
-    {
-	pthread_mutex_lock(&bucklocks[h]);
-	Session *session = buckets[h];
-	Session *parent = 0;
-	while (session)
-	{
-	    if (sessionExpired(*session, now))
-	    {
-		Session *tmp = session->next;
-		if (parent) parent->next = session->next;
-		else buckets[h] = session->next;
-		deleteSession(session);
-		session = tmp;
-	    }
-	    else
-	    {
-		parent = session;
-		session = session->next;
-	    }
-	}
-	pthread_mutex_unlock(&bucklocks[h]);
-    }
+    pthread_mutex_lock(&sessionlock);
+    PSC_Dictionary_removeAll(sessions, isExpired, &now);
+    pthread_mutex_unlock(&sessionlock);
 }
 
 Session *Session_get(const HttpContext *context)
 {
     return HttpContext_get(context, PROPNAME);
-}
-
-const char *Session_id(const Session *self)
-{
-    return self->id;
 }
 
 const char *Session_referrer(const Session *self)
@@ -228,18 +124,16 @@ const char *Session_referrer(const Session *self)
 void *Session_getProp(Session *self, const char *name)
 {
     pthread_mutex_lock(&self->lock);
-    SessionProp *prop = findProp(self, name, 0);
+    void *obj = PSC_Dictionary_get(self->props, name, strlen(name));
     pthread_mutex_unlock(&self->lock);
-    return prop ? prop->value : 0;
+    return obj;
 }
 
 void Session_setProp(Session *self, const char *name,
 	void *obj, ObjDeleter deleter)
 {
     pthread_mutex_lock(&self->lock);
-    SessionProp *prop = insertOrUpdate(self, name);
-    prop->value = obj;
-    prop->deleter = deleter;
+    PSC_Dictionary_set(self->props, name, strlen(name), obj, deleter);
     pthread_mutex_unlock(&self->lock);
 }
 
@@ -251,11 +145,8 @@ void MW_SessionOpts_addLimit(uint16_t seconds, uint16_t limit)
 
 void MW_Session_init(void)
 {
+    pthread_mutex_init(&sessionlock, 0);
     pthread_mutex_init(&cleanlock, 0);
-    for (unsigned i = 0; i < SHT_SIZE; ++i)
-    {
-	pthread_mutex_init(&bucklocks[i], 0);
-    }
     cleantime = time(0);
     if (!createLimitOpts)
     {
@@ -265,6 +156,7 @@ void MW_Session_init(void)
 	RateLimitOpts_addLimit(createLimitOpts, 3600, 25);
     }
     createLimit = RateLimit_create(createLimitOpts);
+    sessions = PSC_Dictionary_create(deleteSession);
 }
 
 void MW_Session(HttpContext *context)
@@ -280,7 +172,7 @@ void MW_Session(HttpContext *context)
     time_t now = time(0);
     clean(now);
     const char *sid = Cookies_getCookie(cookies, COOKIENAME);
-    if (sid) self = findSession(sid, now, 0);
+    if (sid) self = findSession(sid, now);
     if (self)
     {
 	self->atime = now;
@@ -296,8 +188,9 @@ void MW_Session(HttpContext *context)
 		    HttpResponse_createError(HTTP_TOOMANYREQUESTS, 0));
 	    return;
 	}
-	self = createSession(now);
-	Cookies_setCookie(cookies, COOKIENAME, self->id);
+	char newsid[SID_LEN+1];
+	self = createSession(now, newsid);
+	Cookies_setCookie(cookies, COOKIENAME, newsid);
     }
     HttpContext_set(context, PROPNAME, self, 0);
 
@@ -326,17 +219,8 @@ void MW_Session_done(void)
     RateLimitOpts_destroy(createLimitOpts);
     createLimit = 0;
     createLimitOpts = 0;
-    for (unsigned i = 0; i < SHT_SIZE; ++i)
-    {
-	pthread_mutex_destroy(&bucklocks[i]);
-	Session *s = buckets[i];
-	while (s)
-	{
-	    Session *n = s->next;
-	    deleteSession(s);
-	    s = n;
-	}
-	buckets[i] = 0;
-    }
+    PSC_Dictionary_destroy(sessions);
+    sessions = 0;
     pthread_mutex_destroy(&cleanlock);
+    pthread_mutex_destroy(&sessionlock);
 }
