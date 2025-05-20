35022a4cf65190bfed869092cfc0e2167ab46abf
Felix Palmen <felix@palmen-it.de>
2025-05-20 12:20:59 +0200

    swad.mk: Bump version to 0.10

--- src/bin/swad/swad.mk
+++ src/bin/swad/swad.mk
@@ -23,7 +23,7 @@ EXP_BZERO_S_HEADERS=	strings.h
 EXP_BZERO_S_ARGS=	void *, size_t
 EXP_BZERO_S_RETURN=	void
 
-swad_VERSION=		0.9
+swad_VERSION=		0.10
 swad_MODULES=		authenticator \
 			config \
 			csrfprotect \
